3a43edc32d8a8ba353ad829576f36b6492d2429e
rahmanhamim <rahmanhamiminfo@gmail.com>
2023-03-22 11:29:49 +0600

    product route setup

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,3 +1,12 @@
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ success: true, msg: "show all products" });
+  res.status(200).json({ success: true, msg: "show all products static" });
+};
+
+const getAllProducts = async (req, res) => {
+  res.status(200).json({ success: true, msg: "products route" });
+};
+
+module.exports = {
+  getAllProductsStatic,
+  getAllProducts,
 };
