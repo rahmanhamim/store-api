48ab704b8b485864657e1367306a24cda52a8617
rahmanhamim <rahmanhamiminfo@gmail.com>
2023-03-23 14:33:16 +0600

    sort and select fields added

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,16 +1,14 @@
 const Product = require("../models/Product.model");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({
-    name: "modern bookshelf",
-  });
+  const products = await Product.find({}).sort("-name");
   res
     .status(200)
     .json({ success: true, nbHits: products.length, data: products });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
 
   const queryObject = {};
 
@@ -26,7 +24,26 @@ const getAllProducts = async (req, res) => {
     queryObject.name = { $regex: name, $options: "i" };
   }
 
-  const products = await Product.find(queryObject);
+  let result = Product.find(queryObject); // await result at the end of the query.
+
+  /**
+   * @description sort here await result at the end of the query.
+   */
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  const products = await result;
+
+  // const products = await Product.find(queryObject);
   res
     .status(200)
     .json({ success: true, nbHits: products.length, data: products });
