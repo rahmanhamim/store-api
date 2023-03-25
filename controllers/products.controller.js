4b76a577df21418f6da34d56743c51f142583356
rahmanhamim <rahmanhamiminfo@gmail.com>
2023-03-25 15:25:57 +0600

    pagination added

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -41,12 +41,26 @@ const getAllProducts = async (req, res) => {
     result = result.select(fieldsList);
   }
 
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const products = await result;
 
+  const total = await Product.countDocuments();
+  const totalPages = Math.ceil(total / limit);
   // const products = await Product.find(queryObject);
-  res
-    .status(200)
-    .json({ success: true, nbHits: products.length, data: products });
+  res.status(200).json({
+    success: true,
+    nbHits: products.length,
+    total,
+    totalPages,
+    currentPage: page,
+    limit: limit,
+    data: products,
+  });
 };
 
 module.exports = {
